1382eb0c11827afe0917dd00ec64e939c808df35
Deniz Mutlu <96434352+deniz1976@users.noreply.github.com>
2022-11-10 14:26:46 +0300

    Add files via upload
    the algorithms those we will use in our project later.

---
+++ CSEFLIX/CSEFLIX.cpp
@@ -0,0 +1,41 @@
+/**
+ * Deniz Mutlu
+ * Ali Kagan Yilmaz
+ * Kerem Ar
+ * Berkay Cefakar
+ * Burak Mike
+ * Yaren Can
+ *
+ * CSEFLIX sorting and finding algorithms using std::sort and std::find will be implemented here.
+ * These algorithms will be used in the main project to sort and find movies and series in node.js
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+
+#pragma once
+
+extern "C" {
+/**
+ * include the header file for the node.js addon
+ * /include/node/
+ */
+
+int main(int argc, char *argv[]) {
+    return 0;
+}
+}
+
