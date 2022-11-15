1d7f364a17c7140c02a3964a96596c0c9c1abb17
Ali Kağan Yılmaz <91954535+MiqeWazowhiskey@users.noreply.github.com>
2022-11-15 22:04:39 +0300

    feature: layout sample.

---
+++ src/components/common/Layout.js
@@ -0,0 +1,18 @@
+import React from 'react'
+const Layout = ({children}) => {
+  return (
+    <div class='container'>
+        <div className='header'>
+            {/**Header */}Header
+        </div>
+        <div class='sidebar'>
+            {/** Sidebar */}Sidebar
+        </div>
+        <div class='children'>
+          { children }
+        </div>
+
+    </div>
+  )
+}
+export default Layout
