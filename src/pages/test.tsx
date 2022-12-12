b0c519ed4ac27e5eb9148b50d2ac5975704abb1b
Burak Mike <burakmahyk@gmail.com>
2022-12-12 18:41:01 +0300

    Create test.tsx

---
+++ src/pages/test.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import MovieCard from '../components/MovieCard/MovieCard'
+
+const test = () => {
+  return (
+    <div className="testPage">
+      <MovieCard
+        name="Mad Max: Fury Road"
+        release_date="2015-05-13"
+        voteAvarage={7.567}
+        genres={[
+          {
+            id: 28,
+            name: 'Action',
+          },
+          {
+            id: 12,
+            name: 'Adventure',
+          },
+          {
+            id: 878,
+            name: 'Science Fiction',
+          },
+        ]}
+      />
+    </div>
+  )
+}
+
+export default test
