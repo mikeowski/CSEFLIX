3119bd9cd21ea582748423662fc0c5791ae3c42d
Burak Mike <burakmahyk@gmail.com>
2022-12-15 21:29:24 +0300

    refactor: multiple classes with classnames

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,22 +1,16 @@
 import css from './index.module.css'
 import React from 'react'
+import classNames from 'classnames'
 const Header = () => {
   return (
     <header className={css.header}>
       <div className={css.logo}>
-        CSEFLIX
+        <img src="./cseflixLogo.png" />
       </div>
-      <div className={css.navItem}>
-        Popular
-      </div>
-      <div className={css.navItem}>
-        Categories
-      </div>
-      <div className={css.searchBox}>
-        Search
-      </div>
-     
-      </header>
+      <div className={classNames(css.navItem, css.selected)}>Popular</div>
+      <div className={css.navItem}>Categories</div>
+      <div className={css.searchBox}>Search</div>
+    </header>
   )
 }
 export default Header
