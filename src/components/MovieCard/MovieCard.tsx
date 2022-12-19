e1853892f24f6afe63a7ab03f6bed3bd9399218d
Deniz Mutlu <denizmine38@hotmail.com>
2022-12-19 19:13:53 +0300

    isReleased function is written and html

--- src/components/MovieCard/MovieCard.tsx
+++ src/components/MovieCard/MovieCard.tsx
@@ -1,21 +1,36 @@
 import css from './index.module.css'
 interface PropType {
   name: string
-  voteAvarage: number
   release_date: string
   genres: { id: number; name: string }[]
 }
 
-const MovieCard = ({ name, voteAvarage, release_date, genres }: PropType) => {
-  const upcoming = true
+const MovieCard = ({ name, release_date, genres }: PropType) => {
+  let counter = 0;
+  let today = new Date();
+  let releaseDate = Date.parse(release_date);
+  let upcoming = ""
+  function isReleased() {
+    if (today.getTime() >= releaseDate) {
+      upcoming = "";
+    }
+    else {
+      upcoming = "Upcoming";
+    }
+  }
   return (
     <div>
-      <h1>{name}</h1>
-      <div className={css.bg}>
-        {genres.map((genre) => {
-          return <div key={genre.id}>{genre.name}</div>
-        })}
-        {upcoming ? <div></div> : null}
+      <div id="image">
+        <img className="peaky-img" src=" https://m.media-amazon.com/images/M/MV5BMThlOWE3MWEtZjM4Ny00M2FiLTkyMmYtZGY3ZTcyMzM5YmNlXkEyXkFqcGdeQWpnYW1i._V1_.jpg" alt="" />
+        <p className="movie-text release-text">{upcoming}</p>
+        <div className="container">
+          <div className="genre-div">
+            <p className="movie-text genre">{genres[0]!.name}</p>
+            <p className="movie-text genre">{genres[1]!.name}</p>
+            <p className="movie-text genre">{genres[2]!.name} </p>
+          </div>
+          <p className="movie-text movie-name">{name}</p>
+        </div>
       </div>
     </div>
   )
