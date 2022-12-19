06a7bfd8acf1e03915e6cd8abed860a6a5df38de
Kerem AR <keremar0000@gmail.com>
2022-12-19 19:49:23 +0300

    added props, fix upcoming, fix classname

--- src/components/MovieCard/MovieCard.tsx
+++ src/components/MovieCard/MovieCard.tsx
@@ -18,18 +18,18 @@ const MovieCard = ({ name, release_date, genres }: PropType) => {
       upcoming = "Upcoming";
     }
   }
+  isReleased()
   return (
     <div>
-      <div id="image">
-        <img className="peaky-img" src=" https://m.media-amazon.com/images/M/MV5BMThlOWE3MWEtZjM4Ny00M2FiLTkyMmYtZGY3ZTcyMzM5YmNlXkEyXkFqcGdeQWpnYW1i._V1_.jpg" alt="" />
+      <div className="movie-card">
+        <img className="movie-img" src=" https://m.media-amazon.com/images/M/MV5BMThlOWE3MWEtZjM4Ny00M2FiLTkyMmYtZGY3ZTcyMzM5YmNlXkEyXkFqcGdeQWpnYW1i._V1_.jpg" alt="" />
         <p className="movie-text release-text">{upcoming}</p>
-        <div className="container">
-          <div className="genre-div">
-            <p className="movie-text genre">{genres[0]!.name}</p>
-            <p className="movie-text genre">{genres[1]!.name}</p>
-            <p className="movie-text genre">{genres[2]!.name} </p>
-          </div>
-          <p className="movie-text movie-name">{name}</p>
+        <div className="movieCardContainer">
+            <div className="genre-div">
+               {genres.map((genre) => ( <p key={genre.id} className="movie-text genre">{genre.name} </p>))}
+            </div>
+            <p className="movie-text movie-name">{name}</p>
+
         </div>
       </div>
     </div>
