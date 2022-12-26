7374ea99baa7d3e53afcfc91197be9cb61bf2447
Burak Mike <burakmahyk@gmail.com>
2022-12-26 20:38:18 +0300

    feat: created popular page

---
+++ src/pages/popular.tsx
@@ -0,0 +1,11 @@
+import { Layout } from '../components/common'
+
+const Popular = () => {
+  return (
+    <Layout title="Popular">
+      <div></div>
+    </Layout>
+  )
+}
+
+export default Popular
