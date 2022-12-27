aa69844192971013a3781d6c99d8b1c6c919ad03
Burak Mike <burakmahyk@gmail.com>
2022-12-27 22:21:46 +0300

    refactor

--- src/components/MovieCard/MovieCard.tsx
+++ src/components/MovieCard/MovieCard.tsx
@@ -1,19 +1,18 @@
 import Image from 'next/image'
+import { Movie } from '../../types'
 import { trpc } from '../../utils/trpc'
 import css from './index.module.css'
 interface PropType {
-  name: string
-  release_date: string
-  genreIds: number[]
-  imageUrl: string
+  movie: Movie
 }
 
-const MovieCard = ({ name, release_date, genreIds, imageUrl }: PropType) => {
+const MovieCard = ({ movie }: PropType) => {
   const {
     data: genres,
     isLoading,
     isSuccess,
-  } = trpc.movieRouter.getGenresByIds.useQuery({ genreIds: genreIds })
+  } = trpc.movieRouter.getGenresByIds.useQuery({ genreIds: movie.genre_ids })
+  const { original_title: name, release_date, backdrop_path: imageUrl } = movie
   const counter = 0
   const today = new Date()
   const _releaseDate = Date.parse(release_date)
@@ -27,26 +26,24 @@ const MovieCard = ({ name, release_date, genreIds, imageUrl }: PropType) => {
   }
   isReleased()
   return (
-    <div>
-      <div className="movie-card">
-        <Image
-          className="movie-img"
-          src={`https://image.tmdb.org/t/p/original${imageUrl}`}
-          alt={name}
-          layout="fill"
-        />
-        <p className="movie-text release-text">{upcoming}</p>
-        <div className="movieCardContainer">
-          <div className="genre-div">
-            {isSuccess &&
-              genres.map((genre) => (
-                <p key={genre.id} className="movie-text genre">
-                  {genre.name}
-                </p>
-              ))}
-          </div>
-          <p className="movie-text movie-name">{name}</p>
+    <div className="movie-card">
+      <Image
+        className="movie-img"
+        src={`https://image.tmdb.org/t/p/original${imageUrl}`}
+        alt={name}
+        layout="fill"
+      />
+      <p className="movie-text release-text">{upcoming}</p>
+      <div className="movieCardContainer">
+        <div className="genre-div">
+          {isSuccess &&
+            genres.map((genre) => (
+              <p key={genre.id} className="movie-text genre">
+                {genre.name}
+              </p>
+            ))}
         </div>
+        <p className="movie-text movie-name">{name}</p>
       </div>
     </div>
   )
