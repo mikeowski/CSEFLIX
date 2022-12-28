30de50d4f08657783fb3421b646114ec43560c1b
Burak Mike <burakmahyk@gmail.com>
2022-12-29 02:16:44 +0300

    feat: added movie detail response type and People Response type

--- src/types/index.ts
+++ src/types/index.ts
@@ -22,6 +22,51 @@ export interface Movie {
   vote_average: number
   vote_count: number
 }
+export interface MovieDetailedResponse {
+  adult: boolean
+  backdrop_path: string
+  belongs_to_collection: any
+  budget: number
+  genres: Genre[]
+  homepage: string
+  id: number
+  imdb_id: string
+  original_language: string
+  original_title: string
+  overview: string
+  popularity: number
+  poster_path: string
+  production_companies: ProductionCompany[]
+  production_countries: ProductionCountry[]
+  release_date: string
+  revenue: number
+  runtime: number
+  spoken_languages: SpokenLanguage[]
+  status: string
+  tagline: string
+  title: string
+  video: boolean
+  vote_average: number
+  vote_count: number
+}
+
+export interface ProductionCompany {
+  id: number
+  logo_path: string
+  name: string
+  origin_country: string
+}
+
+export interface ProductionCountry {
+  iso_3166_1: string
+  name: string
+}
+
+export interface SpokenLanguage {
+  english_name: string
+  iso_639_1: string
+  name: string
+}
 
 export type Genre = {
   id: number
@@ -63,4 +108,56 @@ export interface MovieVideo {
   type: string
   official: boolean
   id: string
+}
+
+export interface PeopleResponse {
+  id: number
+  cast: Cast[]
+  crew: Crew[]
+}
+
+export interface Cast {
+  adult: boolean
+  gender: number
+  id: number
+  known_for_department: string
+  name: string
+  original_name: string
+  popularity: number
+  profile_path?: string
+  cast_id: number
+  character: string
+  credit_id: string
+  order: number
+}
+
+export interface Crew {
+  adult: boolean
+  gender: number
+  id: number
+  known_for_department: string
+  name: string
+  original_name: string
+  popularity: number
+  profile_path?: string
+  credit_id: string
+  department: string
+  job: string
+}
+
+export interface People {
+  adult: boolean
+  also_known_as: any[]
+  biography: string
+  birthday: string
+  deathday: any
+  gender: number
+  homepage: any
+  id: number
+  imdb_id: string
+  known_for_department: string
+  name: string
+  place_of_birth: string
+  popularity: number
+  profile_path: string
 }
