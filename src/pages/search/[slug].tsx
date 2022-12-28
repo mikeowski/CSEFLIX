ee2d29ef7f8c9d5b0efdba98ee8cd9eaa4c31581
Burak Mike <burakmahyk@gmail.com>
2022-12-29 02:15:45 +0300

    refactor: styling

--- src/pages/search/[slug].tsx
+++ src/pages/search/[slug].tsx
@@ -1,5 +1,6 @@
 import { useRouter } from 'next/router'
 import { Layout } from '../../components/common'
+import Loading from '../../components/Loading/Loading'
 import MovieCard from '../../components/MovieCard/MovieCard'
 import { trpc } from '../../utils/trpc'
 
@@ -12,19 +13,22 @@ const Popular = () => {
 
   return (
     <Layout>
-      {isLoading && <div>LOADİNG</div>}
-      {isError && <div>ERROR </div>}
-      {isSuccess && data && (
-        <div className="mt-20 flex flex-wrap gap-8 justify-center">
-          {isLoading && <div>LOADİNG</div>}
-          {isError && <div>ERROR </div>}
-          {isSuccess &&
-            data &&
-            data.map(
-              (movie) => movie.backdrop_path && <MovieCard movie={movie} />
-            )}
-        </div>
-      )}
+      <div className="mt-44 font-black text-center text-9xl font-header text-white">
+        <div>{slug}</div>
+
+        {isLoading && <Loading />}
+        {isError && <div>ERROR </div>}
+        {isSuccess && data && (
+          <div className="mt-20 flex flex-wrap gap-8 justify-center">
+            {isError && <div>ERROR </div>}
+            {isSuccess &&
+              data &&
+              data.map(
+                (movie) => movie.backdrop_path && <MovieCard movie={movie} />
+              )}
+          </div>
+        )}
+      </div>
     </Layout>
   )
 }
