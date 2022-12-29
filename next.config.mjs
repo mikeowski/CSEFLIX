b7785a0f53c521501340d10a7fe680a2389e43d8
Burak Mike <burakmahyk@gmail.com>
2022-12-29 11:16:01 +0300

    fix: base path

--- next.config.mjs
+++ next.config.mjs
@@ -14,6 +14,8 @@ function defineNextConfig(config) {
 }
 
 export default defineNextConfig({
+  basePath: '',
+
   reactStrictMode: true,
   swcMinify: true,
   // Next.js i18n docs: https://nextjs.org/docs/advanced-features/i18n-routing
